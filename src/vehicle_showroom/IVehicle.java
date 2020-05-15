ee1c9546300918cf85112fe1ce5019fa7ff0ffef
Md. Sharzul Mostafa <sharzulrakib@gmail.com>
2020-05-15 21:20:23 +0600

    Add all files

---
+++ src/vehicle_showroom/IVehicle.java
@@ -0,0 +1,8 @@
+package vehicle_showroom;
+
+
+public interface IVehicle {
+    void addVehicle();
+    void details();
+    void removeVehicle();
+}
---
+++ src/vehicle_showroom/Vehicle_Showroom.java
@@ -0,0 +1,15 @@
+package vehicle_showroom;
+
+import java.util.*;
+
+public class Vehicle_Showroom {
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.menuDisplayMenu();
+        
+        menu.menuChooseOption();
+    }
+    
+}
